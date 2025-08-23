2b8c73b085814fbbb92011a281e5e2f76674d3b8
Tanmay Maheshwari <tanmaym06@proton.me>
2025-08-23 10:42:27 +0530

    style: format the core.opts

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,15 +3,12 @@ local opt = vim.opt
 vim.cmd("let g:netrw_banner = 0")
 vim.cmd("syntax on")
 
-
 -- For line number
 opt.number = true
 opt.relativenumber = true
 
-
 -- For clipboard
 opt.clipboard = "unnamedplus"
-
 
 -- For indentation & stuff like that
 opt.autoindent = true
@@ -20,8 +17,7 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-opt.backspace = {"start", "eol", "indent"}
-
+opt.backspace = { "start", "eol", "indent" }
 
 -- For searching & highlighting
 opt.incsearch = true
@@ -29,14 +25,12 @@ opt.inccommand = "split"
 opt.hlsearch = true
 opt.ignorecase = true
 opt.smartcase = true
-vim.cmd [[highlight LineNr term=underline ctermfg=12 guifg=blue]]
-
+vim.cmd([[highlight LineNr term=underline ctermfg=12 guifg=blue]])
 
 -- For backups & swaps
 opt.swapfile = false
 opt.backup = false
 opt.undofile = true
-
 
 -- UI Stuff
 opt.wrap = false
