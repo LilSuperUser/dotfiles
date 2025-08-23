d23f242cd6f9e97d6956791af0fe3728de14927e
Tanmay Maheshwari <tanmaym06@proton.me>
2025-08-23 09:46:26 +0530

    feat: add conform.nvim in formatting.lua to format files

---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,80 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile", "BufReadPost" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters = {
+				["markdown-toc"] = {
+					condition = function(_, ctx)
+						for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+							if line:find("<!%-%- toc %-%->") then
+								return true
+							end
+						end
+					end,
+				},
+				["markdownlint-cli2"] = {
+					condition = function(_, ctx)
+						local diag = vim.tbl_filter(function(d)
+							return d.source == "markdownlint"
+						end, vim.diagnostic.get(ctx.buf))
+						return #diag > 0
+					end,
+				},
+			},
+			formatters_by_ft = {
+				markdown = { "prettier", "markdown-toc" },
+				lua = { "stylua" },
+				html = { "biome-check", "prettier" },
+				css = { "biome-check", "prettier" },
+				javascript = { "biome-check" },
+				typescript = { "biome-check" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				python = { "black" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				rust = { "rustfmt" },
+				-- ["markdown.mdx"] = { "prettier", "markdownlint", "markdown-toc" },
+			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
+		})
+
+		-- Configure individual formatters
+		conform.formatters.prettier = {
+			args = {
+				"--stdin-filepath",
+				"$FILENAME",
+				"--tab-width",
+				"4",
+				"--use-tabs",
+				"false",
+			},
+		}
+
+		conform.formatters["clang-format"] = {
+			prepend_args = {
+				-- Pass all style options within a single string
+				"--style={BasedOnStyle: file, IndentWidth: 4, UseTab: Never}",
+			},
+		}
+
+		conform.formatters.shfmt = {
+			prepend_args = { "-i", "4" },
+		}
+
+		vim.keymap.set({ "n", "v" }, "<leader>mf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format whole file or range (in visual mode) with" })
+	end,
+}
