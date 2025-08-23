6d33b7d8819c5d069fc38dfd1adb6f374298b9a0
Tanmay Maheshwari <tanmaym06@proton.me>
2025-08-23 10:34:02 +0530

    feat: add keymaps for:
    - toggling LSP diagnostics
    - dismiss all notification windows currently displayed
    - also format the keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -17,19 +17,33 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "<ove up in buffer with cursor 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down in buffer with cursor centered" })
 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Move to next match, center it on screen and ensure its visible if folded" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Move to previous match, center it on screen and ensure its visible if folded" })
+vim.keymap.set(
+	"n",
+	"N",
+	"Nzzzv",
+	{ desc = "Move to previous match, center it on screen and ensure its visible if folded" }
+)
 
 vim.keymap.set("n", "<leader>C", ":nohl<CR>", { desc = "Clear search hl", silent = true })
 
 vim.keymap.set("v", "<", "<gv", { desc = "Indent selection to left and keep selection", noremap = true, silent = true })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent selection to right and keep selection", noremap = true, silent = true })
+vim.keymap.set(
+	"v",
+	">",
+	">gv",
+	{ desc = "Indent selection to right and keep selection", noremap = true, silent = true }
+)
 
 -- format without prettier, using the built in shit
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format current buffer with LSP" })
 
 -- Replace the word that the cursor is on globally
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Replace the current word globally" })
+vim.keymap.set(
+	"n",
+	"<leader>r",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Replace the current word globally" }
+)
 
 -- Makes the current file executable
 vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>")
@@ -39,13 +53,25 @@ vim.keymap.set("n", "<leader>XX", "<cmd>!chmod +x % && ./%<CR>")
 
 -- Copy file path to the clipboard
 vim.keymap.set("n", "<leader>fp", function()
-        local filePath = vim.fn.expand("%:~")
-        vim.fn.setreg("+", filePath)
-        print("File path copied to clipboard: " .. filePath)
-    end,
-    { desc = "Copy the path of the file in the clipboard" }
-)
+	local filePath = vim.fn.expand("%:~")
+	vim.fn.setreg("+", filePath)
+	print("File path copied to clipboard: " .. filePath)
+end, { desc = "Copy the path of the file in the clipboard" })
 
+-- Toggle LSP diagnostics visibility
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+	isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+	vim.diagnostic.config({
+		virtual_text = isLspDiagnosticsVisible,
+		underline = isLspDiagnosticsVisible,
+	})
+end, { desc = "Toggle LSP diagnostics" })
+
+-- Dismiss all notification windows currently displayed
+vim.keymap.set("n", "<leader>cn", function()
+	require("notify").dismiss({ silent = false, pending = false })
+end, { noremap = true, desc = "Clear notification" })
 
 -----------------------------------------------------
 -------------- Clipboard Stuff ----------------------
@@ -65,7 +91,6 @@ vim.keymap.set({ "n", "v" }, "<leader>Ds", [["_d]])
 -- Same as above but for a whole line
 vim.keymap.set({ "n", "v" }, "<leader>Dl", [["_dd]])
 
-
 -----------------------------------------------------
 ----------------- Tab Stuff -------------------------
 -----------------------------------------------------
@@ -75,19 +100,17 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close a tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Move to next tab" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Move to previous tab" })
 
-
 -----------------------------------------------------
 -------------- Autocommands Stuff -------------------
 -----------------------------------------------------
 -- Higlight what was yanked
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking stuff",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	desc = "Highlight when yanking stuff",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
-
 
 -----------------------------------------------------
 ----------------- splits Stuff ----------------------
